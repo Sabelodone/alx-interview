fec1268181dbf2ecdaa9eb89fcd712615647ae14
Sabelodone <sabelozondo825@gmail.com>
2024-03-07 15:11:07 +0000

    Create a function def pascal_triangle(n): that returns a list of lists of integers representing the adds triangle of n:Pascal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for row in range(n):
+        current_row = []
+        for col in range(row + 1):
+            if col == 0 or col == row:
+                current_row.append(1)
+            else:
+                # Calculate element using combination formula
+                element = triangle[row - 1][col - 1] + triangle[row - 1][col]
+                current_row.append(element)
+        triangle.append(current_row)
+
+    return triangle
+
+def print_triangle(triangle):
+    for row in triangle:
+        print("[{}]".format(",".join(map(str, row))))
+
+if __name__ == "__main__":
+    triangle = pascal_triangle(5)
+    print_triangle(triangle)
