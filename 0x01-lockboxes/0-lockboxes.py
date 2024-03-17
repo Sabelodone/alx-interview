7a8840cdee6bb85f7509a657beae3141ea6a3c23
Sabelodone <sabelozondo825@gmail.com>
2024-03-17 11:01:37 +0000

    method that determines if all the boxes can be opened.

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,20 +4,22 @@ Solution to lockboxes problem
 """
 
 def canUnlockAll(boxes):
-    """
-    Determines whether a series of locked boxes can be opened
-    based on keys that can be attained.
-    Solution to the lockboxes problem
-    """
     if not isinstance(boxes, list) or len(boxes) == 0:
         return False
-    
-    for key in range(1, len(boxes)):
-        found = False
-        for idx, box in enumerate(boxes):
-            if key in box and key != idx:
-                found = True
-                break
-        if not found:
-            return False
-    return True
+
+    # Set to keep track of opened boxes
+    opened_boxes = {0}
+
+    # Queue to perform BFS traversal of boxes
+    queue = [0]
+
+    # Perform BFS traversal
+    while queue:
+        box_idx = queue.pop(0)
+        for key in boxes[box_idx]:
+            if key < len(boxes) and key not in opened_boxes:
+                opened_boxes.add(key)
+                queue.append(key)
+
+    # Check if all boxes can be opened
+    return len(opened_boxes) == len(boxes)
