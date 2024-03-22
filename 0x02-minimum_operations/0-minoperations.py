a9c252d3c368d1e8b03d32add2a7449566942b9e
Sabelodone <sabelozondo825@gmail.com>
2024-03-22 13:56:34 +0200

    minimum number of operations for each given n.

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python3
-def minOperations(n):
+def minOperations(n: int) -> int:
+    """
+    Calculate the minimum number of operations needed to obtain 'n' H characters.
+    """
     if n <= 1:
         return 0
     
-    min_ops = 0
-    divisor = 2
+    operations = 0
+    copied_chars = 'H'  # Initially, there's only one 'H' in the text file
     
-    while n > 1:
-        while n % divisor == 0:
-            min_ops += 1
-            n //= divisor
-        divisor += 1
+    while len(copied_chars) < n:
+        if n % len(copied_chars) == 0:
+            # If the current length of the string is a factor of n, we can double the string
+            operations += 2
+            copied_chars *= 2
+        else:
+            # Otherwise, we append the content already in the text file
+            operations += 1
+            copied_chars += copied_chars
     
-    return min_ops
+    # If the final length of the string matches n, return the number of operations
+    if len(copied_chars) == n:
+        return operations
+    else:
+        return 0  # If it's impossible to achieve 'n' characters, return 0
