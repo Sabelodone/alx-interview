dd68896c155d8be0670d5b415063052960a1f930
Sabelodone <sabelozondo825@gmail.com>
2024-04-05 01:06:13 +0200

    method that determines if a given data set represents a valid UTF-8 encoding.

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,26 +3,30 @@
 
 
 def get_leading_set_bits(num):
-    """Returns the number of leading set bits (1)"""
-    set_bits = bin(num)[::-1].index('1')
+    """returns the number of leading set bits (1)"""
+    set_bits = 0
+    helper = 1 << 7
+    while helper & num:
+        set_bits += 1
+        helper = helper >> 1
     return set_bits
 
 
 def validUTF8(data):
-    """Determines if a given data set represents a valid UTF-8 encoding"""
+    """determines if a given data set represents a valid UTF-8 encoding"""
     bits_count = 0
-    for byte in data:
+    for i in range(len(data)):
         if bits_count == 0:
-            bits_count = get_leading_set_bits(byte)
-            # 1-byte (format: 0xxxxxxx)
+            bits_count = get_leading_set_bits(data[i])
+            '''1-byte (format: 0xxxxxxx)'''
             if bits_count == 0:
                 continue
-            # A character in UTF-8 can be 1 to 4 bytes long
+            '''a character in UTF-8 can be 1 to 4 bytes long'''
             if bits_count == 1 or bits_count > 4:
                 return False
         else:
-            # Checks if current byte has format 10xxxxxx
-            if not (byte & (1 << 7) and not (byte & (1 << 6))):
+            '''checks if current byte has format 10xxxxxx'''
+            if not (data[i] & (1 << 7) and not (data[i] & (1 << 6))):
                 return False
         bits_count -= 1
     return bits_count == 0
