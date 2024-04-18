d800a900017d731c2f97b945b7b8ef190867db3a
Sabelodone <sabelozondo825@gmail.com>
2024-04-18 15:27:05 +0200

    Star Wars Characters1

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,50 +1,42 @@
 #!/usr/bin/node
 
-const request = require('request');
-
-const movieId = process.argv[2];
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Failed to fetch movie data:', response.statusCode);
-    return;
-  }
-
-  const movieData = JSON.parse(body);
-  const characters = movieData.characters;
-
-  // Create an array to store promises for fetching character data
-  const characterPromises = characters.map(characterUrl => {
+/**
+ * Wrapper function for request object that allows it
+ * to work with async and await
+ * @param   {String} url - site url
+ * @returns {Promise}    - promise object that resolves
+ *                         with parsed JSON response
+ *                         and rejects with the request error.
+ */
+function makeRequest (url) {
+    const request = require('request');
     return new Promise((resolve, reject) => {
-      request(characterUrl, (error, response, body) => {
-        if (error) {
-          reject('Error fetching character data: ' + error);
-          return;
-        }
-
-        if (response.statusCode !== 200) {
-          reject('Failed to fetch character data: ' + response.statusCode);
-          return;
-        }
-
-        const characterData = JSON.parse(body);
-        resolve(characterData.name);
+      request.get(url, (error, response, body) => {
+        if (error) reject(error);
+        else resolve(JSON.parse(body));
       });
     });
-  });
+  }
+  
+  /**
+   * Entry point - makes requests to Star Wars API
+   * for movie info based movie ID passed as a CLI parameter.
+   * Retrieves movie character info then prints their names
+   * in order of appearance in the initial response.
+   */
+  async function main () {
+    const args = process.argv;
+    if (args.length < 3) return;
+    const movieUrl = 'https://swapi-api.alx-tools.com/api/films/' + args[2];
 
-  // Wait for all character requests to complete
-  Promise.all(characterPromises)
-    .then(characterNames => {
-      characterNames.forEach(name => console.log(name));
-    })
-    .catch(error => {
-      console.error(error);
-    });
-});
+    const movie = await makeRequest(movieUrl);
+    if (movie.characters === undefined) return;
+  
+    for (const characterUrl of movie.characters) {
+      const character = await makeRequest(characterUrl);
+      console.log(character.name);
+    }
+  }
+  
+  main();
+  
