1fac092e803522216865454969b958cef8e1ffd4
Sabelodone <sabelozondo825@gmail.com>
2024-05-02 20:57:14 +0200

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
+""" Making changes """
+
 
 def makeChange(coins, total):
     """ Generate changes needed to reach total
 
     Args:
-        coins (List[int]): List of coin denominations available.
-        total (int): Total amount needed to reach.
-
-    Returns:
-        int: Minimum number of coins needed to reach the total amount.
-             Returns -1 if it's not possible to reach the total amount.
+        coins ([List]): [List of Coins available]
+        total ([int]): [total amount needed]
     """
-    if total == 0:
+    if total <= 0:
         return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
+    check = 0
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total:
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
+    return -1
